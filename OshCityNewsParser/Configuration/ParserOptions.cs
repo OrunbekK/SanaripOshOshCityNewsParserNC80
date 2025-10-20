7b116ec9fb57cfd4ade7587bc1d330cf1c1b5c7b
OrunbekK <orunbek@gmail.com>
2025-10-21 00:24:51 +0600

    step 05 phase 02

--- OshCityNewsParser/Configuration/ParserOptions.cs
+++ OshCityNewsParser/Configuration/ParserOptions.cs
@@ -2,10 +2,17 @@
 {
     internal class ParserOptions
     {
-        public string NewsPageUrl { get; set; } = string.Empty;
+        public List<LanguageConfig> Languages { get; set; } = new();
         public int HttpTimeoutSeconds { get; set; } = 30;
         public int RateLimitRequestsPerSecond { get; set; } = 2;
         public int RetryPolicyAttempts { get; set; } = 3;
         public int RetryDelayMilliseconds { get; set; } = 1000;
+    }
+
+    internal class LanguageConfig
+    {
+        public string Code { get; set; } = string.Empty;
+        public string NewsPageUrl { get; set; } = string.Empty;
+        public byte LanguageUID { get; set; }
     }
 }
