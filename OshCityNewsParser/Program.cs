b556551f7956456a532698c9b8456028a02aa1bd
OrunbekK <orunbek@gmail.com>
2025-10-20 23:14:16 +0600

    Add project files.

---
+++ OshCityNewsParser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OshCityNewsParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
